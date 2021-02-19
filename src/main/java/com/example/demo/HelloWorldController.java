fee339cf33638f519ac0d29903feaaaa0c21414d
Edgy Santana <tesseus2006@gmail.com>
2021-02-18 20:33:18 -0500

    Uploading a project to GitHub

---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("api/hello-world")
+@RestController
+public class HelloWorldController {
+
+    // GET, PUT, POST, DELETE
+
+    @GetMapping
+    public List<String> getHelloWorld() {
+        return List.of("Hello", "World");
+    }
+}
